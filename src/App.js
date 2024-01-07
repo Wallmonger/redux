5f9c0c333bdbf715c3c556792c8562bff4141241
Wallmonger <nicolas.berrier360@gmail.com>
2024-01-07 23:47:01 +0100

    React-redux / Store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import PhoneComponent from './components/PhoneComponent';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 function App() {
     return (
         <>
+        <Provider store={store}>
             <h1>Hello world</h1>
             <div className="section-one">
                 <PhoneComponent />
             </div>
+        </Provider>
         </>
     );
 }
